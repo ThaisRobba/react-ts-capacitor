127b6806dfa618d1c47e4193f20f698528bbc0f9
ThaisRobba <thais.robba@gmail.com>
2020-09-18 17:33:43 -0300

    fix: add missing head tag on index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Plugins } from "@capacitor/core";
-
-async function share() {
-  const { Share } = Plugins;
-  await Share.share({
-    title: "See cool stuff",
-    text: "Really awesome thing you need to see right meow",
-    url: "http://ionicframework.com/",
-    dialogTitle: "Share with buddies",
-  });
-}
+import { Capacitor, Plugins } from "@capacitor/core";
 
 const App = () => {
+  async function share() {
+    if (!Capacitor.isNative) {
+      console.log("NOT NATIVE");
+    } else {
+      console.log("IS NATIVE!!!");
+    }
+    const { Share } = Plugins;
+    await Share.share({
+      title: "See cool stuff",
+      text: "Really awesome thing you need to see right meow",
+      url: "http://ionicframework.com/",
+      dialogTitle: "Share with buddies",
+    });
+  }
+
   return (
     <div>
       <p>hello2</p>
